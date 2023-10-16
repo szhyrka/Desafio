382c9527116d45093cb2b169ed539b46a9abdc65
Gabriel Waltrich <64818081+szhyrka@users.noreply.github.com>
2023-10-16 20:41:10 -0300

    Desafio Parte 1

---
+++ exercicios/src/teste1/Data.java
@@ -0,0 +1,64 @@
+package teste1;
+public class Data{
+    private int dia, mes, ano;
+    
+    public Data(int dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+        validarData();
+        
+        
+    }
+    
+    public boolean verificaAnoBissexto(){
+        boolean retorno = false;
+        retorno = (this.ano % 4 == 0) && (this.ano % 100 != 0 || this.ano % 400 == 0);
+    
+        return retorno;
+    }
+      
+    private void validarData() {
+        boolean anoBissexto = this.verificaAnoBissexto();
+        int[] diasPorMes = {31, anoBissexto ? 29 : 28, 31, 30, 31, 30, 31 ,31 ,30 ,31, 30 ,31};
+    
+    
+        if (mes < 1 || mes > 12 || dia < 1 || dia > diasPorMes[mes - 1]){
+            System.out.println("[NOK] Data informada está invalida. Alterando para 1/1/2000.");
+            this.dia = 1;
+            this.mes = 1;
+            this.ano = 2000;
+        }
+    }
+    
+    public int getDia(){
+        return dia;
+    }
+    
+    public int getMes(){
+        return mes;
+    }
+    
+    public int getAno(){
+        return ano;
+    }
+    
+    public void setDia(int dia){
+        this.dia = dia;
+    }
+    
+    public void setMes(int mes){
+        this.mes = mes;
+    }
+    
+    public void setAno(int ano){
+        this.ano = ano;
+    }
+    
+    public String toString(){
+        return dia + "/" + mes + "/" + ano;
+    }
+    
+}
+    
+    
---
+++ exercicios/src/teste1/Loja.java
@@ -0,0 +1,73 @@
+package teste1;
+public class Loja{
+    private String nome;
+    private int quantidadeFuncionarios;
+    private double salarioBaseFuncionario;
+    
+    
+    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario){
+        this.nome = nome;
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+    
+    public Loja(String nome, int quantidadeFuncionarios){
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+     
+    public String getNome(){
+        return nome;
+    }
+    
+    public int getQuantidadeFuncionarios(){
+        return quantidadeFuncionarios;
+    }
+    
+    public double getSalarioBaseFuncionario(){
+        return -1;
+    }
+    
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public void setQuantidadeFuncionarios(int quantidadeFuncionarios){
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+    
+    public void setSalarioBaseFuncionario(double salarioBaseFuncionario){
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+    
+    public double gastosComSalario(){
+        if (salarioBaseFuncionario < 1){
+            return -1;
+        }
+        else{ 
+            double gasto = quantidadeFuncionarios * salarioBaseFuncionario;
+            return gasto;
+        }
+    }
+    
+    public char tamanhoDaLoja(){
+      int quantidadeFuncionarios = getQuantidadeFuncionarios();
+
+      if (quantidadeFuncionarios < 10) {
+        return 'P';
+      } else if (quantidadeFuncionarios <= 30) {
+        return 'M';
+      } else {
+        return 'G';
+      }
+    }
+    
+    public String toString(){
+        return "O nome da loja é:" + nome + ", e ela possui" + quantidadeFuncionarios +
+        "funcionarios que recebem" + salarioBaseFuncionario + "de salario base";
+    }
+    
+    
+    
+}
