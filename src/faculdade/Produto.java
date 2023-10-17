bcb944df65ed7dacc17755aca4ecb1984f490cdc
Gabriel Waltrich <64818081+szhyrka@users.noreply.github.com>
2023-10-16 21:14:35 -0300

    Desafio parte1

---
+++ src/faculdade/Produto.java
@@ -0,0 +1,33 @@
+package faculdade;
+public class Produto{
+    private String nome;
+    private double preco;
+    
+    public Produto(String nome, double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public double getPreco(){
+        return preco;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public void setPreco(double preco){
+        this.preco = preco;
+    }
+    
+    public String toString(){
+        return "O nome do produto é" + nome + ", E o valor atualmente dele é" + preco;
+    }
+    
+    
+    
+}
